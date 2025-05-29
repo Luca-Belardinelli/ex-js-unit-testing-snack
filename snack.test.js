b9b2e858ad548c81944c01424d1045612f4deeb7
Luca Belardinelli <belardinelliluca@gmail.com>
2025-05-29 15:14:03 +0200

    snack2

--- snack.test.js
+++ snack.test.js
@@ -1,8 +1,13 @@
 // importo le funzioni
-const { getInitials } = require('./snack')
+const { getInitials, createSlug } = require('./snack')
 
 
 //snack 1
 test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
     expect(getInitials("Luca Belardinelli")).toBe("L.B.");
+});
+
+//snack 2
+test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+    expect(createSlug("Hello Word")).toBe("hello word")
 });
